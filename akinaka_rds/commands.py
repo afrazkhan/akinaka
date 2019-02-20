8e1d0f948e9f98ca0efb67320a3cc7ff04d10688
Choon Ming Goh <choon.ming@choonming.org>
2019-02-20 15:54:43 +0100

    change formatting for cli arguments

--- akinaka_rds/commands.py
+++ akinaka_rds/commands.py
@@ -14,12 +14,12 @@ def rds(ctx, region):
 @click.pass_context
 @click.option("--source-role-arn", required=True, help="Source role ARNs with assumable permissions")
 @click.option("--target-role-arn", required=True, help="Destination role ARNs with assumable permissions")
-@click.option("--snapshot_style", type=click.Choice(['running_instance', 'latest_snapshot']), required=True, help="Use latest available backup or create a new snapshot")
-@click.option("--db_id", required=True, help="RDS DB instance identifier")
-@click.option("--overwrite_target", is_flag=True, help="Specify this parameter to overwrite existing instance")
-@click.option("--security_group", required=True, help="RDS Security to be attached to the target RDS instance")
-@click.option("--db_subnet", required=True, help="RDS DB subnet to be attached to the instance")
-@click.option("--target_instance_name", default=None, help="Name of the newly created RDS instance")
+@click.option("--snapshot-style", type=click.Choice(['running_instance', 'latest_snapshot']), required=True, help="Use latest available backup or create a new snapshot")
+@click.option("--db-id", required=True, help="RDS DB instance identifier")
+@click.option("--overwrite-target", is_flag=True, help="Specify this parameter to overwrite existing instance")
+@click.option("--security-group", required=True, help="RDS Security to be attached to the target RDS instance")
+@click.option("--db-subnet", required=True, help="RDS DB subnet to be attached to the instance")
+@click.option("--target-instance-name", default=None, help="Name of the newly created RDS instance")
 
 def copy(ctx, source_role_arn, target_role_arn, snapshot_style, db_id, overwrite_target, security_group, db_subnet, target_instance_name):
     from .copy import copy_rds
