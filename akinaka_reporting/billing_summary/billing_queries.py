ddeb90beee8d464228fcc62cec04b445b6b5a845
Nazrul Kamaruddin <nazrul@olindata.com>
2019-03-14 22:20:15 +0100

    Replaced default function for command.

--- akinaka_reporting/billing_summary/billing_queries.py
+++ akinaka_reporting/billing_summary/billing_queries.py
@@ -7,28 +7,13 @@ import sys
 import tabulate
 
 class BillingQueries():
-    def __init__(self, region, assume_role_arn):
-        self.cloudwatch = akinaka_libs.cloudwatch.CloudWatch(region, assume_role_arn)
+    def __init__(self, region, assume_role_arn, days_ago=0):
         self.costexplorer = akinaka_libs.costexplorer.CostExplorer(region, assume_role_arn)
-
-    def print_last_two_estimates(self):
-        try:
-            estimates = self.cloudwatch.get_bill_estimates(from_hours_ago=72)
-            sorted_estimates = sorted(estimates, key=lambda k: k['Timestamp'])
-        except Exception as e:
-            print("Couldn't get last two estimates: {}".format(e))
-        
-        message = """\
-Last estimate     ({}): {}
-Previous estimate ({}): {}
-""".format(sorted_estimates[-1]['Timestamp'], sorted_estimates[-1]['Maximum'], sorted_estimates[-2]['Timestamp'], sorted_estimates[-2]['Maximum'])
-
-        print(message)
-        return message
+        self.days_ago = days_ago
 
     def print_x_days_estimates(self):
         try:
-            response = self.costexplorer.get_bill_estimates(from_days_ago=0)
+            response = self.costexplorer.get_bill_estimates(from_days_ago=self.days_ago)
             data = response['ResultsByTime']
         except Exception as e:
             print("Billing estimates is not available: {}".format(e))
