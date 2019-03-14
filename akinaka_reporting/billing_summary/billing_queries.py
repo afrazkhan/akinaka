53f1a13aef9806cbf9c157b2d9739782b45c21c1
Nazrul Kamaruddin <nazrul@olindata.com>
2019-03-14 21:51:56 +0100

    Added table for multiple rows of billing data.

--- akinaka_reporting/billing_summary/billing_queries.py
+++ akinaka_reporting/billing_summary/billing_queries.py
@@ -4,6 +4,7 @@ import boto3
 import time
 import datetime
 import sys
+import tabulate
 
 class BillingQueries():
     def __init__(self, region, assume_role_arn):
@@ -26,13 +27,26 @@ Previous estimate ({}): {}
         return message
 
     def print_x_days_estimates(self):
-        data = ""
         try:
-            response = self.costexplorer.get_bill_estimates(from_days_ago=0)
+            response = self.costexplorer.get_bill_estimates(from_days_ago=3)
             data = response['ResultsByTime']
         except Exception as e:
             print("Billing estimates is not available: {}".format(e))
-
-        message = data
+            return e
+        results = []
+        if len(data) == 1:
+            amount = float(data[0]['Total']['UnblendedCost']['Amount'])
+            unit = data[0]['Total']['UnblendedCost']['Unit']
+            date_today = datetime.datetime.now().strftime("%Y-%m-%d")
+            results.append([date_today, "{} {:.2f}".format(unit, amount)])
+            message = "\nToday's estimated bill\n"
+        else:
+            for d in data:
+                unit = d['Total']['UnblendedCost']['Unit']
+                amount = float(d['Total']['UnblendedCost']['Amount'])
+                results.append([d['TimePeriod']['End'], "{} {:.2f}".format(unit, amount)])
+            message = "\nEstimated bill for the past {} days\n".format(str(len(data)))
+        message += tabulate.tabulate(results, headers=["Date", "Total"], tablefmt='psql')
+        message += "\n"
         print(message)
         return message
